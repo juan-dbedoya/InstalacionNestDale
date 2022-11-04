92cf81b0bd17c09130b0c050e3c5129b22410650
Juan Dávila <juan.davila@booktofly.com.co>
2022-11-03 20:28:24 -0500

    ADD

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AppService } from '../Services/app.service';
 
-@Controller()
+@Controller('saludo')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
